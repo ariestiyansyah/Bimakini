2d53a6c2ff0bdf3f156eb2db84f12de97f0d130a
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-12-30 11:08:08 +0700

    initializing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bimakini
+
+Bimakini.com client
---
+++ providers/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_URL:string = "http://www.bimakini.com";
+export const GOOGLE_ANALYTICS_ID:string ="UA-5341016-19";
+export const GCM_SENDER_ID:string = "";
